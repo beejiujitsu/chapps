362dd906db7e2714420d29316c2d8c3da095bfa1
Mark Shields <mshields@twitter.com>
2018-06-15 10:03:39 -0400

    app -> click, remove log use

--- chaps.py
+++ chaps.py
@@ -15,35 +15,23 @@ from chaps import lib
 
 import click
 
-log.LogOptions().disable_disk_logging()
-
 
 @click.command(name="binary")
+@click.argument('args', nargs=-1)
 def binary_goal(args):
   """Create a binary using pants."""
 
   targets = lib.targets(lib.rel_cwd(), args)
-
-  if targets is None:
-    app.error("No target given.")
-
-  log.debug("chaps targets: %s", targets)
 
   pants_args = "binary {0}".format(targets)
   lib.pants(pants_args)
 
 
 @click.command(name="fmt")
+@click.argument('args', nargs=-1)
 def fmt_goal(args):
   """Fix common format issues using pants fmt goal."""
-
   targets = lib.targets(lib.rel_cwd(), args)
-
-  if targets is None:
-    app.error("No target given.")
-
-  log.debug("chaps targets: %s", targets)
-
   pants_args = "fmt {0}".format(targets)
   lib.pants(pants_args)
 
@@ -53,38 +41,26 @@ def list_goal():
   """List relative path pants targets."""
   path = lib.rel_cwd()
   pants_args = "list {0}:".format(path)
-
   lib.pants_list(pants_args)
 
 
 @click.command(name="repl")
+@click.argument('args', nargs=-1)
 def repl_goal(args):
   """Enter an ipython REPL."""
-
   targets = lib.targets(lib.rel_cwd(), args)
-
-  if targets is None:
-    app.error("No target given.")
-
-  log.debug("chaps targets: %s", targets)
-
   pants_args = "repl --repl-py-ipython {0}".format(targets)
   lib.pants(pants_args)
 
 
 @click.command(name="run")
-def run_goal(args):
+@click.argument('goal')
+@click.argument('args', nargs=-1)
+def run_goal(goal, args):
   """Run a target using pants."""
 
-  single_target = args[0]
-  targets = lib.targets(lib.rel_cwd(), [single_target])
-
-  if targets is None:
-    app.error("No target given.")
-
-  run_args = " ".join(args[1:])
-
-  log.debug("chaps targets: %s", targets)
+  targets = lib.targets(lib.rel_cwd(), [goal])
+  run_args = " ".join(args)
 
   pants_args = "run {0} {1}".format(targets, run_args)
   lib.pants(pants_args)
@@ -93,10 +69,8 @@ def run_goal(args):
 @click.command(name="clean")
 @click.command(name="clean-all")
 @click.command(name="wash")
-def clean_goal(args):
+def clean_goal():
   """Clean pants."""
-
-  log.debug("chaps clean")
   lib.pants("clean-all")
 
 
@@ -122,15 +96,9 @@ def test_goal(args, options):
   else:
     targets = lib.targets(lib.rel_cwd(), args)
 
-  log.debug("chaps targets: %s", targets)
-
   pants_args = "test.pytest  {0} {1} {2}".format(
     "--coverage=%d" % int(options.coverage),
     "--test-pytest-options='%s'" % lib.pytest_options(options),
     targets,
   )
   lib.pants(pants_args)
-
-
-app.set_usage_based_on_commands()
-app.main()
