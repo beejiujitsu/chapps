bcc1931b5ad3a7389864d491418f5be1902070df
Mark Shields <mshields@twitter.com>
2018-06-15 11:07:14 -0400

    chaps -> pchaps, naming collision, add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+  name="pchaps",
+  version="1.0",
+  py_modules=["pchaps", "chaps_lib"],
+  install_requires=["Click", "sarge", ],
+  entry_points={
+    "console_scripts": ["chaps=pchaps:main"],
+  }
+)
